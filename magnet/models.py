a8d550bc8793ed5969f0a8e561aa6ead57ad7ed9
Vaisakh <svaisakh1994@gmail.com>
2018-05-01 16:47:23 +0530

    Allow Using Functions in Sequential Model

--- magnet/models.py
+++ magnet/models.py
@@ -1,12 +1,14 @@
-import torch
-
 from torch import nn
-
-from .nodes import Node
 
 class Sequential(nn.Sequential):
     def __init__(self, *layers, input_shape):
-        for layer in layers:
+        from .nodes import Node, Lambda
+        from inspect import isfunction
+
+        layers = list(layers)
+        for i, layer in enumerate(layers):
+            if isfunction(layer): layers[i] = Lambda(layer)
+
             if isinstance(layer, Node):
                 layer.build(input_shape)
                 input_shape = layer.get_output_shape(input_shape)
