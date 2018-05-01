2d55b957f88ce64ae6305ba4b35ec858b6fac9f5
Vaisakh <svaisakh1994@gmail.com>
2018-05-01 19:19:21 +0530

    Refactor Linear to be a MonoNode (++)
    Also fix ancestor issue (again!)

--- magnet/_utils.py
+++ magnet/_utils.py
@@ -1,20 +1,21 @@
 def caller_locals(ancestor=False):
     """Print the local variables in the caller's frame."""
     import inspect
-    frame = inspect.currentframe().f_back
+    frame = inspect.currentframe().f_back.f_back
     
     try:
-        l = frame.f_back.f_locals
-        if not ancestor:
-            true_caller = l.pop('self', None)
-            true_class = l.pop('__class__', None)
-        else:
-            f_class = None
+        l = frame.f_locals
+        
+        if ancestor:
+            f_class = l.pop('__class__', None)
             caller = l.pop('self')
-            while f_class != caller.__class__:
-                frame = frame.f_back
+            while f_class is not None and isinstance(caller, f_class):
                 l = frame.f_locals
-                f_class = l.pop('__class__', None)
                 l.pop('self', None)
+                frame = frame.f_back
+                f_class = frame.f_locals.pop('__class__', None)
+
+        l.pop('self', None)
+        l.pop('__class__', None)
         return l
     finally: del frame
--- magnet/nodes.py
+++ magnet/nodes.py
@@ -18,8 +18,8 @@ class Node(nn.Module):
 
     def _parse_params(self):
         args = caller_locals(ancestor=True)
-        for k in ('args', 'kwargs'):
-            if k not in args.keys(): args[k] = []
+        if 'args' not in args.keys(): args['args'] = []
+        if 'kwargs' not in args.keys(): args['kwargs'] = {}
         
         default_param_list = list(self._default_params.items())
 
@@ -41,26 +41,63 @@ class Node(nn.Module):
     def get_output_shape(self, in_shape):
         with torch.no_grad(): return tuple(self(torch.randn(in_shape)).size())
 
-class Conv(Node):
+class MonoNode(Node):
     def __init__(self, *args, **kwargs):
         super().__init__()
         self._set_activation()
 
     def build(self, in_shape):
+        self._layer_class = self._find_layer(in_shape)
+        kwargs = self._get_kwargs(in_shape)
+        self._layer = self._layer_class(**kwargs)
+
+    def forward(self, x):
+        return self._activation(self._layer(x))
+
+    def _find_layer(self, in_shape):
+        pass
+
+    def _get_kwargs(self, in_shape):
+        return {k: self._args[v] for k, v in self._kwargs_dict.items()}
+
+    @property
+    def _kwargs_dict(self):
+        pass
+
+    def _set_activation(self):
+        from torch.nn import functional as F
+        from functools import partial
+
+        activation_dict = {'relu': F.relu, 'sigmoid': F.sigmoid, 'tanh': F.tanh,
+                            'lrelu': partial(F.leaky_relu, leak=0.2), None: lambda x: x}
+        self._activation = activation_dict[self._args['act']]
+
+    @property
+    def _default_params(self):
+        p = {'act': 'relu'}
+        p.update(super()._default_params)
+        return p
+
+class Conv(MonoNode):
+    def build(self, in_shape):
+        self._set_padding(in_shape)
+
+        super().build(in_shape)
+
+    def _find_layer(self, in_shape):
         shape_dict = [nn.Conv1d, nn.Conv2d, nn.Conv3d]
         ndim = len(in_shape) - 2
-        module = shape_dict[ndim - 1]
-        
-        self._set_padding(in_shape)
-        
-        kw_map = {'kernel_size': 'k', 'out_channels': 'c','stride': 's',
-                 'padding': 'p', 'dilation': 'd', 'groups': 'g', 'bias': 'b'}
-        kwargs = {k: self._args[v] for k, v in kw_map.items()}
+        return shape_dict[ndim - 1]
+
+    def _get_kwargs(self, in_shape):
+        kwargs = super()._get_kwargs(in_shape)
         kwargs['in_channels'] = in_shape[1]
-        self.conv = module(**kwargs)
-        
-    def forward(self, x):
-        return self._activation(self.conv(x))
+        return kwargs
+
+    @property
+    def _kwargs_dict(self):
+        return {'kernel_size': 'k', 'out_channels': 'c','stride': 's',
+                'padding': 'p', 'dilation': 'd', 'groups': 'g', 'bias': 'b'}
     
     def _set_padding(self, in_shape):
         p = self._args['p']
@@ -80,41 +117,31 @@ class Conv(Node):
         
     @property
     def _default_params(self):
-        return {'c': None, 'k': 3, 'p': 'half', 's': 1, 'd': 1, 'g': 1, 'b': True, 'act': 'relu'}
+        p = {'c': None, 'k': 3, 'p': 'half', 's': 1, 'd': 1, 'g': 1, 'b': True}
+        p.update(super()._default_params)
+        return p
 
-    def _set_activation(self):
-        from torch.nn import functional as F
-        from functools import partial
-
-        activation_dict = {'relu': F.relu, 'sigmoid': F.sigmoid, 'tanh': F.tanh,
-                            'lrelu': partial(F.leaky_relu, leak=0.2), None: lambda x: x}
-        self._activation = activation_dict[self._args['act']]
-
-class Linear(Node):
+class Linear(MonoNode):
     def __init__(self, o, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._set_activation()
 
-    def build(self, in_shape):
-        kw_map = {'out_features': 'o', 'bias': 'b'}
-        kwargs = {k: self._args[v] for k, v in kw_map.items()}
+    def _find_layer(self, in_shape):
+        return nn.Linear
+
+    def _get_kwargs(self, in_shape):
+        kwargs = super()._get_kwargs(in_shape)
         kwargs['in_features'] = in_shape[-1]
-        self.fc = nn.Linear(**kwargs)
+        return kwargs
 
-    def forward(self, x):
-        return self._activation(self.fc(x))
+    @property
+    def _kwargs_dict(self):
+        return {'out_features': 'o', 'bias': 'b'}
 
     @property
     def _default_params(self):
-        return {'b': True, 'act': 'relu'}
-
-    def _set_activation(self):
-        from torch.nn import functional as F
-        from functools import partial
-
-        activation_dict = {'relu': F.relu, 'sigmoid': F.sigmoid, 'tanh': F.tanh,
-                            'lrelu': partial(F.leaky_relu, leak=0.2), None: lambda x: x}
-        self._activation = activation_dict[self._args['act']]
+        p = {'b': True, 'act': 'relu'}
+        p.update(super()._default_params)
+        return p
 
 class Lambda(Node):
     def __init__(self, fn):
