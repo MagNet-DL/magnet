41286867296bf8561e4ac845a34f5bf0a58106c6
Vaisakh <svaisakh1994@gmail.com>
2018-06-02 04:15:43 +0000

    Fix Device Initialization Typo

--- magnet/__init__.py
+++ magnet/__init__.py
@@ -1,5 +1,9 @@
 from .autograd import eval, device
 
-if device == 'gpu': print('Accelerating your code on shiney new', torch.cuda.get_device_name(0), 'GPU.')
-else: print("Running your code on a slow, boring CPU.\nMake some money and buy yourself a GPU, will ya?"
+def __print_init_message():
+	from torch.cuda import get_device_name
+	if device == 'cuda': print('Accelerating your code on shiney new', get_device_name(0), 'GPU.')
+	else: print("Running your code on a slow, boring CPU.\nMake some money and buy yourself a GPU, will ya?"
             "\n\nPro Tip: If you're a poor old hag like me, use a cloud provider.")
+	
+__print_init_message()
