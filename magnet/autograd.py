d46aa450b0909d942a99a76c999105a466d13193
Vaisakh <svaisakh1994@gmail.com>
2018-06-07 14:31:53 +0530

    Fix eval() Decorator Issue
    When used as a decorator without arguments, eval() would refuse
    to find Modules.
    
    This was simply because of the use of type(x) is y construct
    rather than isinstance(x, y).

--- magnet/autograd.py
+++ magnet/autograd.py
@@ -5,35 +5,35 @@ from contextlib import contextmanager
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def eval(*modules):
-    """A Context Manager that makes it easy to run statements in eval mode.
-    It sets ```modules``` in eval() mode and ensures that gradients are not computed"""
-    
-    from inspect import isfunction
-    if not isfunction(modules[0]) or len(modules) > 1:
-        return _eval_context_manager(*modules)
-    
-    from functools import wraps
+	"""A Context Manager that makes it easy to run statements in eval mode.
+	It sets ```modules``` in eval() mode and ensures that gradients are not computed"""
 
-    fn = modules[0]
-    @wraps(fn)
-    def new_fn(*args, **kwargs):
-        from torch.nn import Module
-        
-        arg_list = list(args) + list(kwargs.values())
-        modules = [a for a in arg_list if type(a) is Module]
-        
-        with _eval_context_manager(*modules): 
-            return fn(*args, **kwargs)
-            
-    return new_fn
+	from inspect import isfunction
+	if not isfunction(modules[0]) or len(modules) > 1:
+		return _eval_context_manager(*modules)
+
+	from functools import wraps
+
+	fn = modules[0]
+	@wraps(fn)
+	def new_fn(*args, **kwargs):
+		from torch.nn import Module
+
+		arg_list = list(args) + list(kwargs.values())
+		modules = [a for a in arg_list if isinstance(a, Module)]
+
+		with _eval_context_manager(*modules):
+			return fn(*args, **kwargs)
+
+	return new_fn
 
 @contextmanager
 def _eval_context_manager(*modules):
-    states = []
-    for module in modules:
-        states.append(module.training)
-        module.eval()
-        
-    with torch.no_grad(): yield
+	states = []
+	for module in modules:
+		states.append(module.training)
+		module.eval()
 
-    for module, state in zip(modules, states): module.train(state)
+	with torch.no_grad(): yield
+
+	for module, state in zip(modules, states): module.train(state)
