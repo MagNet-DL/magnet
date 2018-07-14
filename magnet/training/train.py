06cfe1cd396521121e4eeb7ca53593b5cbedd2a6
Vaisakh <svaisakh1994@gmail.com>
2018-07-14 13:32:54 +0530

    Remove losses and metrics from base Trainer

--- magnet/training/train.py
+++ magnet/training/train.py
@@ -2,16 +2,12 @@ import magnet as mag
 from contextlib import contextmanager
 
 class Trainer:
-	def __init__(self, models, losses=None, optimizers=['adam'], metrics=None):
-		from magnet.training import metrics as metrics_module
-
+	def __init__(self, models, optimizers=['adam']):
 		from torch import optim
 
 		self.models = models
-		self.losses = losses
 		if optimizers[0] == 'adam': optimizers = [optim.Adam(model.parameters(), amsgrad=True) for model in models]
-		self._optimizers = optimizers
-		self._metrics = {metrics: getattr(metrics_module, metrics.lower())} if metrics is not None else {}
+		self.optimizers = optimizers
 
 		self.iterations = 0
 
@@ -91,7 +87,7 @@ class Trainer:
 		from magnet.training.utils import load_state, load_object
 
 		for i, model in enumerate(self.models): load_state(model, path / 'models', alternative_name=str(i))
-		for i, optimizer in enumerate(self._optimizers): load_state(optimizer, path / 'optimizers', alternative_name=str(i))
+		for i, optimizer in enumerate(self.optimizers): load_state(optimizer, path / 'optimizers', alternative_name=str(i))
 
 		state_dict = load_object(path / 'state.p', default={})
 		for attr, val in state_dict.items(): setattr(self, attr, val)
@@ -102,7 +98,7 @@ class Trainer:
 		from magnet.training.utils import save_state, save_object
 
 		for i, model in enumerate(self.models): save_state(model, path / 'models', alternative_name=str(i))
-		for i, optimizer in enumerate(self._optimizers): save_state(optimizer, path / 'optimizers', alternative_name=str(i))
+		for i, optimizer in enumerate(self.optimizers): save_state(optimizer, path / 'optimizers', alternative_name=str(i))
 
 		state_dict = {attr: getattr(self, attr) for attr in ('iterations', ) if hasattr(self, attr)}
 		save_object(state_dict, path / 'state.p')
@@ -110,13 +106,19 @@ class Trainer:
 		self.callbacks('save', trainer=self, path=path / 'callbacks')
 
 class SupervisedTrainer(Trainer):
-	def __init__(self, model, loss=None, optimizer='adam', metrics=None):
-		super().__init__([model], [loss], [optimizer], metrics)
+	def __init__(self, model, optimizer='adam', loss='cross_entropy', metric=None):
+		from magnet.training import metrics as metrics_module
+		from torch.nn import functional as F
+
+		super().__init__([model], [optimizer])
+		loss_wiki = {'cross_entropy': F.cross_entropy}
+		self.loss = loss_wiki[loss]
+		self.metric = (metric, getattr(metrics_module, metric.lower())) if metric is not None else None
 
 	def optimize(self):
-		model = self.models[0]; optimizer = self._optimizers[0]
+		model = self.models[0]; optimizer = self.optimizers[0]
 
-		loss = self._get_loss(self.dataloader)
+		loss = self.get_loss(self.dataloader)
 
 		if self.training:
 			loss.backward()
@@ -126,26 +128,24 @@ class SupervisedTrainer(Trainer):
 
 	@staticmethod
 	def validate(trainer, dataloader):
-		trainer._get_loss(dataloader, validation=True)
+		trainer.get_loss(dataloader, validation=True)
 
-	def _get_loss(self, dataloader, validation=False):
-		model = self.models[0]; loss_fn = self.losses[0]
+	def get_loss(self, dataloader, validation=False):
+		model = self.models[0]
 
 		mode = 'val' if validation else 'train'
 
 		x, y = next(dataloader)
 		y_pred = model(x)
 
-		loss = loss_fn(y_pred, y)
+		loss = self.loss(y_pred, y)
 
 		self.callbacks('write_metrics', trainer=self, key='loss', value=loss.item(), validation=validation, buffer=True)
-		for k in self._metrics.keys():
-			self.callbacks('write_metrics', trainer=self, key=k, value=self._metrics[k](y_pred, y).item(), validation=validation, buffer=True)
+		if self.metric is not None:
+			self.callbacks('write_metrics', trainer=self, key=self.metric[0], value=self.metric[1](y_pred, y).item(), validation=validation, buffer=True)
 
 		return loss
 
 class ClassifierTrainer(SupervisedTrainer):
 	def __init__(self, model, optimizer='adam'):
-		from torch import nn
-
-		super().__init__(model, nn.CrossEntropyLoss(), optimizer, metrics='accuracy')
+		super().__init__(model, optimizer, metric='accuracy')
