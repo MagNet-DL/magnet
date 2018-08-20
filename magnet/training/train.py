a1f903cb7e76a075eba59782d204b5385381863f
Vaisakh <svaisakh1994@gmail.com>
2018-08-20 14:24:34 +0530

    Generalize Loss and Metric in SupervisedTrainer

--- magnet/training/train.py
+++ magnet/training/train.py
@@ -4,123 +4,128 @@ from torch import optim
 from contextlib import contextmanager
 
 class Trainer:
-	def __init__(self, models, optimizers):
-		self.models = models
-		self.optimizers = optimizers
+    def __init__(self, models, optimizers):
+        self.models = models
+        self.optimizers = optimizers
 
-		self.iterations = 0
+        self.iterations = 0
 
-	def optimize(self):
-		raise NotImplementedError
+    def optimize(self):
+        raise NotImplementedError
 
-	def train(self, dataloader, epochs=1, callbacks=[], **kwargs):
-		from magnet.training.callbacks import CallbackQueue
+    def train(self, dataloader, epochs=1, callbacks=[], **kwargs):
+        from magnet.training.callbacks import CallbackQueue
 
-		self.dataloader = dataloader
-		self.callbacks = CallbackQueue(callbacks)
+        self.dataloader = dataloader
+        self.callbacks = CallbackQueue(callbacks)
 
-		total_iterations = kwargs.get('iterations', int(epochs * len(dataloader)))
+        total_iterations = kwargs.get('iterations', int(epochs * len(dataloader)))
 
-		self.callbacks('on_training_start', trainer=self, total_iterations=total_iterations)
-		for self.iterations in range(self.iterations, self.iterations + total_iterations): next(self)
-		self.callbacks('on_training_end', trainer=self)
+        self.callbacks('on_training_start', trainer=self, total_iterations=total_iterations)
+        for self.iterations in range(self.iterations, self.iterations + total_iterations): next(self)
+        self.callbacks('on_training_end', trainer=self)
 
-	def __iter__(self):
-		return self
+    def __iter__(self):
+        return self
 
-	def __next__(self):
-		self.callbacks('on_batch_start', trainer=self)
-		self.optimize()
-		self.callbacks('on_batch_end', trainer=self)
+    def __next__(self):
+        self.callbacks('on_batch_start', trainer=self)
+        self.optimize()
+        self.callbacks('on_batch_end', trainer=self)
 
-	@contextmanager
-	def mock(self, path=None):
-		from shutil import rmtree
+    @contextmanager
+    def mock(self, path=None):
+        from shutil import rmtree
 
-		if path is None:
-			from pathlib import Path
-			path = Path('.mock_trainer')
+        if path is None:
+            from pathlib import Path
+            path = Path('.mock_trainer')
 
-		self.save_state(path)
-		yield
-		self.load_state(path)
+        self.save_state(path)
+        yield
+        self.load_state(path)
 
-		rmtree(path)
+        rmtree(path)
 
-	def epochs(self, mode=None):
-		if mode is None:
-			return self.iterations / len(self.dataloader)
-		if mode == 'start':
-			return (self.iterations / len(self.dataloader)).is_integer()
-		if mode == 'end':
-			return ((self.iterations + 1) / len(self.dataloader)).is_integer()
+    def epochs(self, mode=None):
+        if mode is None:
+            return self.iterations / len(self.dataloader)
+        if mode == 'start':
+            return (self.iterations / len(self.dataloader)).is_integer()
+        if mode == 'end':
+            return ((self.iterations + 1) / len(self.dataloader)).is_integer()
 
-	def is_training(self):
-		return all(model.training for model in self.models)
+    def is_training(self):
+        return all(model.training for model in self.models)
 
-	def load_state(self, path=None):
-		from magnet.training.utils import load_state, load_object
+    def load_state(self, path=None):
+        from magnet.training.utils import load_state, load_object
 
-		for i, model in enumerate(self.models): load_state(model, path / 'models', alternative_name=str(i))
-		for i, optimizer in enumerate(self.optimizers): load_state(optimizer, path / 'optimizers', alternative_name=str(i))
+        for i, model in enumerate(self.models): load_state(model, path / 'models', alternative_name=str(i))
+        for i, optimizer in enumerate(self.optimizers): load_state(optimizer, path / 'optimizers', alternative_name=str(i))
 
-		state_dict = load_object(path / 'state.p', default={})
-		for attr, val in state_dict.items(): setattr(self, attr, val)
+        state_dict = load_object(path / 'state.p', default={})
+        for attr, val in state_dict.items(): setattr(self, attr, val)
 
-		try: self.callbacks('load_state', trainer=self, path=path / 'callbacks')
-		except AttributeError: pass
+        try: self.callbacks('load_state', trainer=self, path=path / 'callbacks')
+        except AttributeError: pass
 
-	def save_state(self, path=None):
-		from magnet.training.utils import save_state, save_object
+    def save_state(self, path=None):
+        from magnet.training.utils import save_state, save_object
 
-		for i, model in enumerate(self.models): save_state(model, path / 'models', alternative_name=str(i))
-		for i, optimizer in enumerate(self.optimizers): save_state(optimizer, path / 'optimizers', alternative_name=str(i))
+        for i, model in enumerate(self.models): save_state(model, path / 'models', alternative_name=str(i))
+        for i, optimizer in enumerate(self.optimizers): save_state(optimizer, path / 'optimizers', alternative_name=str(i))
 
-		state_dict = {attr: getattr(self, attr) for attr in ('iterations', ) if hasattr(self, attr)}
-		save_object(state_dict, path / 'state.p')
+        state_dict = {attr: getattr(self, attr) for attr in ('iterations', ) if hasattr(self, attr)}
+        save_object(state_dict, path / 'state.p')
 
-		try: self.callbacks('save_state', trainer=self, path=path / 'callbacks')
-		except AttributeError: pass
+        try: self.callbacks('save_state', trainer=self, path=path / 'callbacks')
+        except AttributeError: pass
 
 class SupervisedTrainer(Trainer):
-	def __init__(self, model, optimizer='adam', loss='cross_entropy', metric=None):
-		from magnet.functional import wiki
-		from torch.nn import functional as F
+    def __init__(self, model, optimizer='adam', loss='cross_entropy', metric=None):
+        from magnet.functional import wiki
+        from torch.nn import functional as F
 
-		if isinstance(optimizer, str): optimizer = optimizer_wiki[optimizer](model.parameters())
+        if isinstance(optimizer, str): optimizer = optimizer_wiki[optimizer.lower()](model.parameters())
+        if isinstance(loss, str): loss = wiki['losses'][loss.lower()]
+        if isinstance(metric, str): metric = (metric, wiki['metrics'][metric.lower()])
 
-		super().__init__([model], [optimizer])
+        super().__init__([model], [optimizer])
 
-		self.loss = wiki['losses'][loss]
-		self.metric = (metric, wiki['metrics'][metric.lower()]) if metric is not None else None
+        self.loss = loss
+        self.metric = metric
+        
 
-	def optimize(self):
-		optimizer = self.optimizers[0]
+    def optimize(self):
+        optimizer = self.optimizers[0]
 
-		loss = self.get_loss(self.dataloader)
+        loss = self.get_loss(self.dataloader)
 
-		if self.is_training():
-			loss.backward()
-			self.callbacks('gradient', trainer=self, models=self.models)
-			optimizer.step()
-			optimizer.zero_grad()
+        if self.is_training():
+            loss.backward()
+            self.callbacks('gradient', trainer=self, models=self.models)
+            optimizer.step()
+            optimizer.zero_grad()
 
-	@staticmethod
-	def validate(trainer, dataloader):
-		trainer.get_loss(dataloader, validation=True)
+    @staticmethod
+    def validate(trainer, dataloader):
+        trainer.get_loss(dataloader, validation=True)
 
-	def get_loss(self, dataloader, validation=False):
-		model = self.models[0]
+    def get_loss(self, dataloader, validation=False):
+        def write_stats(key, value):
+            self.callbacks('write_stats', trainer=self, key=key, value=value, validation=validation, buffer_size=len(dataloader))
 
-		x, y = next(dataloader)
-		y_pred = model(x)
+        model = self.models[0]
 
-		loss = self.loss(y_pred, y)
+        x, y = next(dataloader)
+        y_pred = model(x)
 
-		self.callbacks('write_stats', trainer=self, key='loss', value=loss.item(), validation=validation, buffer_size=len(dataloader))
-		if self.metric is not None:
-			self.callbacks('write_stats', trainer=self, key=self.metric[0], value=self.metric[1](y_pred, y).item(), validation=validation, buffer_size=len(dataloader))
+        loss = self.loss(y_pred, y)
 
-		return loss
+        write_stats('loss', loss.item())
+        if self.metric is not None: write_stats(self.metric[0], self.metric[1](y_pred, y).item())
+
+        return loss
 
 optimizer_wiki = {'adam': optim.Adam}
