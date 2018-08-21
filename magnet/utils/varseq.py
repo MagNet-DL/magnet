76d68ea29d90c21cbce7622f75b5289681d6a7a4
Vaisakh <svaisakh1994@gmail.com>
2018-08-21 15:46:43 +0530

    Attach order and unpack() to packed object

--- magnet/utils/varseq.py
+++ magnet/utils/varseq.py
@@ -3,19 +3,27 @@ import torch, numpy as np, magnet as mag
 from torch.nn.utils.rnn import pack_sequence, pad_packed_sequence, pack_padded_sequence
 
 def pack(sequences, lengths=None):
+    from types import MethodType
+
     if lengths is None:
         lengths = list(map(len, sequences))
         order = np.argsort(lengths)[::-1]
         sequences = [sequences[i] for i in order]
-        return pack_sequence(sequences), order
+        sequences = pack_sequence(sequences)
+        sequences.order = order
+    else:
+        order = np.argsort(lengths)[::-1]
+        sequences = sequences[:, order]
+        lengths = lengths[order]
+        sequences = pack_padded_sequence(sequences, torch.tensor(lengths))
 
-    order = np.argsort(lengths)[::-1]
-    sequences = sequences[:, order]
-    lengths = lengths[order]
+    sequences.order = order
+    sequences.unpack = MethodType(lambda self, as_list=False: unpack(self, as_list), sequences)
+    return sequences
 
-    return pack_padded_sequence(sequences, torch.tensor(lengths)), order
+def unpack(sequence, as_list=False):
+    order = sequence.order
 
-def unpack(sequence, order, as_list=False):
     sequences, lengths = pad_packed_sequence(sequence)
     order = np.argsort(order)
 
