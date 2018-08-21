a4fb7d208c6f6bd2988f4e0aaca5952723af6f03
Vaisakh <svaisakh1994@gmail.com>
2018-08-21 13:23:23 +0530

    Add Variable Sequence Packing

---
+++ magnet/utils/varseq.py
@@ -0,0 +1,17 @@
+import torch, numpy as np, magnet as mag
+
+from torch.nn.utils.rnn import pack_sequence, pad_packed_sequence, pack_padded_sequence
+
+def pack(sequences, lengths=None):
+    if lengths is None:
+        lengths = list(map(len, sequences))
+        order = np.argsort(lengths)[::-1]
+        sequences = [sequences[i] for i in order]
+        return pack_sequence(sequences), order
+
+    order = np.argsort(lengths)[::-1]
+    sequences = sequences[:, order]
+    lengths = lengths[order]
+
+    return pack_padded_sequence(sequences, torch.tensor(lengths)), order
+
